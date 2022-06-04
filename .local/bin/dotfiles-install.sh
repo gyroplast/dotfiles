82aee1bf1461b2689d3771a254c2d6729b96ea4e
Dennis Herbrich <dennis.herbrich+codesign@veloxis.de>
2022-06-04 23:25:02 +0000

    feat: add update functionality to install script

--- .local/bin/dotfiles-install.sh
+++ .local/bin/dotfiles-install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-git clone --bare gyroplast@vixen:.dotfiles "${HOME}"/.dotfiles
 function config {
 	git --git-dir="${HOME}"/.dotfiles/ --work-tree="${HOME}" $@
 }
-mkdir -p .config-backup
+git clone --bare gyroplast@vixen:.dotfiles "${HOME}"/.dotfiles --recursive
+if (( $? == 128 )); then
+	printf -- "Refreshing dotfile repo\n"
+	config pull --recurse-submodules
+fi
+mkdir -p "${HOME}"/.config-backup
 config checkout
 if [ $? = 0 ]; then
 	echo "Checked out dotfiles."
 else
 	echo "Backing up pre-existing dot files."
-	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} "${HOME}"/.config-backup/{}
 fi
 config checkout
 config config status.showUntrackedFiles no
+config config commit.gpgsign false
