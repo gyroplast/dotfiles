59a9c2cac3ab9f30e368b717e461f35e0f29889b
Dennis Herbrich <dennis.herbrich+codesign@veloxis.de>
2022-06-09 23:53:43 +0200

    fix(install): add argument for dotfile source repo

--- .local/bin/dotfiles-install.sh
+++ .local/bin/dotfiles-install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
+_dotfile_repo="${1}"
 function config {
 	git --git-dir="${HOME}"/.dotfiles/ --work-tree="${HOME}" $@
 }
-git clone --bare gyroplast@vixen:.dotfiles "${HOME}"/.dotfiles --recursive
+git clone --bare "${_dotfile_repo}" "${HOME}"/.dotfiles --recursive
 if (( $? == 128 )); then
 	printf -- "Refreshing dotfile repo\n"
 	config pull --recurse-submodules
 fi
-mkdir -p "${HOME}"/.config-backup
-config checkout
-if [ $? = 0 ]; then
-	echo "Checked out dotfiles."
-else
-	echo "Backing up pre-existing dot files."
-	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} "${HOME}"/.config-backup/{}
-fi
-config checkout
 config config status.showUntrackedFiles no
 config config commit.gpgsign false
+config checkout
+if (( $? == 0 )); then
+	echo "Checked out dotfiles."
+else
+	echo "Backup existing dotfiles, then run command again:"
+	config checkout 2>&1
+fi
