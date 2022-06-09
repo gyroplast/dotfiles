0bc290a001f848b7f944940a71b8b18e0032bb43
Dennis Herbrich <dennis.herbrich+codesign@veloxis.de>
2022-06-10 00:43:38 +0200

    feat(install): add branch selection to dotfile installer

--- .local/bin/dotfiles-install.sh
+++ .local/bin/dotfiles-install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 _dotfile_repo="${1}"
+_branch="${2:-common}"
 function config {
 	git --git-dir="${HOME}"/.dotfiles/ --work-tree="${HOME}" $@
 }
-git clone --bare "${_dotfile_repo}" "${HOME}"/.dotfiles --recursive
+git clone --branch "${_branch}" --bare "${_dotfile_repo}" "${HOME}"/.dotfiles --recursive
 if (( $? == 128 )); then
 	printf -- "Refreshing dotfile repo\n"
 	config pull --recurse-submodules
 fi
 config config status.showUntrackedFiles no
 config config commit.gpgsign false
-config checkout
+config checkout "${_branch}"
 if (( $? == 0 )); then
 	echo "Checked out dotfiles."
 else
 	echo "Backup existing dotfiles, then run command again:"
-	config checkout 2>&1
+	config checkout "${_branch}" 2>&1
 fi
